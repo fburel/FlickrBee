e786d85e362ce20a9c3ebf3e58bced43ec6b2051
florian BUREL <florian.burel@mac.com>
2015-09-25 03:22:55 +0200

    implementation de la classe UrlDataCache

--- FlickrBee/UrlDataCache.swift
+++ FlickrBee/UrlDataCache.swift
@@ -10,20 +10,39 @@ import Foundation
 
 class UrlDataCache {
     
-    
+    private var cachedData = [NSURL : NSData]()
     
     func hasDataCachedForUrl(url:NSURL) -> Bool
     {
-        
+        return (cachedData[url] != nil)
     }
     
     func dataForUrl(url:NSURL) -> NSData?
     {
-        
+        return cachedData[url]
     }
     
-    func fetchDataForUrl(url:NSURL, (NSData?)->Void)
+    func fetchDataForUrl(url:NSURL, completion:(NSData?)->Void)
     {
+        var qos = DISPATCH_QUEUE_PRIORITY_HIGH
+        var global_queue = dispatch_get_global_queue(qos, 0)
         
+        dispatch_async(global_queue){
+            
+            let d = NSData(contentsOfURL: url)
+            
+            dispatch_async(dispatch_get_main_queue()){
+                
+                
+                if let data = d
+                {
+                  self.cache[url] = data
+                }
+                
+                completion(d)
+                
+                
+            }
+        }
     }
 }
