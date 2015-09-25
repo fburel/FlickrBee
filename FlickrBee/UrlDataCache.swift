029a67bbbf44eec1b0f059bf755d015c932b547b
florian BUREL <florian.burel@mac.com>
2015-09-25 03:16:48 +0200

    creation de la classe UrlDataCache + API

---
+++ FlickrBee/UrlDataCache.swift
@@ -0,0 +1,29 @@
+//
+//  UrlDataCache.swift
+//  FlickrBee
+//
+//  Created by florian BUREL on 25/09/2015.
+//  Copyright (c) 2015 florian BUREL. All rights reserved.
+//
+
+import Foundation
+
+class UrlDataCache {
+    
+    
+    
+    func hasDataCachedForUrl(url:NSURL) -> Bool
+    {
+        
+    }
+    
+    func dataForUrl(url:NSURL) -> NSData?
+    {
+        
+    }
+    
+    func fetchDataForUrl(url:NSURL, (NSData?)->Void)
+    {
+        
+    }
+}
